4553bd33605ffe60f2e18163b9cd47059c47fddd
MLMismas <52412840+MLMismas@users.noreply.github.com>
2019-09-25 08:56:18 -0400

    morning day 3 practice

--- src/app/features/gift-giving/containers/friends/entry/entry.component.ts
+++ src/app/features/gift-giving/containers/friends/entry/entry.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { GiftGivingState } from '../../../reducers';
+import { friendAdded } from '../../../actions/friends.actions';
 
 @Component({
   selector: 'app-friends-entry',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendsEntryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<GiftGivingState>) { }
 
   ngOnInit() {
   }
 
+  addItem(nameEl: HTMLInputElement) {
+    const name = nameEl.value;
+
+    this.store.dispatch(friendAdded({ name }));
+
+    nameEl.value = '';
+    nameEl.focus();
+  }
 }
